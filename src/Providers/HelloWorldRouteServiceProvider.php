9c191ee1641a15ba12327ca14231d4f6ad86b40b
Aldino Said <aldino.said@esphere.id>
2018-02-22 18:03:24 +0700

    just commit

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -12,11 +12,12 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
     public function map(Router $router, ApiRouter $apiRouter)
     {
     	$apiRouter->version(
-						['v1'],
-						['namespace' => 'HelloWorld\Controllers', 'middleware' => 'oauth'],
-						function ($apiRouter) {
-							$apiRouter->get('payment/helloworld/setting/{settingType}', 'ContentController@sayHello');
-						}
+			['v1'],
+			['namespace' => 'HelloWorld\Controllers', 'middleware' => 'oauth'],
+			function ($apiRouter) {
+				$apiRouter->get('payment/helloworld/setting/{settingType}', 'ContentController@sayHello');
+			}
 		);
+		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
     }
 }
